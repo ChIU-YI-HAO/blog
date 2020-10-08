79dc5d11935c4e777c69c521c4f9c8809e4044c2
YI-HAO <D1074181011@gm.lhu.edu.tw>
2020-10-08 14:02:38 +0800

    2020/10/08 route網頁輸入

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/hello', function () {
+    return view('hello');
 });
+Route::get('users/{id}/friends/{slug}', function ($id, $slug) {
+    return $slug ." users"." " . "$id" ."friends.";
+})->where (['id' => '[0-9]+','slug' =>'[A-Za-z]+']);
